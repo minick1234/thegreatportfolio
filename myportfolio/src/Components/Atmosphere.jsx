dc3124abc8a445cd65caecc1827d58e2ebe7a465
Dominik Ziolkowski <minickisbeast@gmail.com>
2024-12-06 20:08:06 -0500

    pushed update

--- myportfolio/src/Components/Atmosphere.jsx
+++ myportfolio/src/Components/Atmosphere.jsx
@@ -5,20 +5,26 @@ import MotionSectionWrapper from './HigherOrderComponent/MotionSectionWrapper.js
 const Atmosphere = () => {
     return (
         <div className="relative w-full">
-            <ScalingStars density={0.02} baseStars={500} />
+            <ScalingStars density={0.01} baseStars={500} />
 
-            <div className="relative z-10 flex flex-col items-center w-full min-h-screen">
-                <div className="flex flex-col items-center justify-center w-full">
-                    <div className="py-8">
-                        {Array.from({ length: 1 }).map((_, idx) => (
-                            <React.Fragment key={idx}>
-                                <h1 className="text-white text-4xl">Atmosphere</h1>
-                                <p className="text-white text-lg mt-4">
-                                    This is some additional content to test the height of the motion section.
-                                </p>
-                            </React.Fragment>
-                        ))}
-                    </div>
+            {/* Foreground Content */}
+            <div className="relative z-10 flex items-center justify-center w-full min-h-screen">
+                {/* Centered Box with 75% Width */}
+                <div className=" w-4/5 max-w-4xl">
+                    <h1 className="text-white text-4xl mb-4 text-center">Education</h1>
+                    <p className="text-white text-lg">
+                        Bachelor of Science in Computer Science
+                    </p>
+                    <p className="text-white text-lg mt-2">
+                        XYZ University, 2022 - Present
+                    </p>
+                  
+                    <p className="text-white text-lg">
+                        High School Diploma
+                    </p>
+                    <p className="text-white text-lg mt-2">
+                        ABC High School, 2018 - 2022
+                    </p>
                 </div>
             </div>
         </div>
